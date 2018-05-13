c6cb886f98c676b5d53d91906eb72bdf94096c9d
Peter Murphy <peterkmurphy@gmail.com>
2018-05-13 16:49:03 +1000

    Missing settings data

--- views.py
+++ views.py
@@ -17,6 +17,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.shortcuts import render
+from django.conf import settings
 
 def index(request):
     return render(request, 'testreact/testreact.html',
